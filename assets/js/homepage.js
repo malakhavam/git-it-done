2880d008a22b99b054714a7779dff5334787319c
Maryia <malakhava@yahoo.com>
2021-12-16 15:56:19 -0500

    finished

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonE1 = document.querySelector("#language-buttons");
 
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -90,3 +91,32 @@ repoEl.appendChild(statusEl);
   repoContainerEl.appendChild(repoEl);
 }
   };
+
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
+
+  var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+      getFeaturedRepos(language);
+    
+      // clear old content
+      repoContainerEl.textContent = "";
+    }
+    console.log(language);
+  }
+
+  languageButtonE1.addEventListener("click", buttonClickHandler);
+
+  getFeaturedRepos();
